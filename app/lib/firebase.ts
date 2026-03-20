825d38bca0731d9a89be522189e09b81a6b4c75f
vishnu <vishnuzealcity@gmail.com>
2026-03-20 13:29:18 +0530

    fix: firebase keys moved to env

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,21 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDj3uN6hLkfWe257z6IfYB0UQXElYbw5hs",
-  authDomain: "arana-developers.firebaseapp.com",
-  projectId: "arana-developers",
-  storageBucket: "arana-developers.firebasestorage.app",
-  messagingSenderId: "556435343276",
-  appId: "1:556435343276:web:e4462308e986ec887ddc09",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// ✅ ADD THESE (THIS FIXES YOUR ERROR)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-
